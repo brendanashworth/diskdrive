d93aeb09ba769652eec5a7a6e422289289af23a0
Brendan Ashworth <squirrelslikeacorns@gmail.com>
2014-11-15 11:32:18 -0800

    Restructure file

--- src/diskdrive.js
+++ src/diskdrive.js
@@ -1,36 +1,35 @@
 var exec = require('child_process').exec;
 
-module.exports = {
-	// id is an OPTIONAL argument.
-	eject: function(id) {
-		// are we running on mac?
-		if (process.platform === 'darwin') {
-			// setup optional argument, on mac, will default to 1
-			id = (typeof id === 'undefined') ? 1 : id;
+var self = module.exports;
 
-			exec('drutil tray eject ' + id, function(err, stdout, stderr) {
-				if (err) {
-					console.log('ERROR: DiskDrive, when executing bash cmd: ' + err);
-					return;
-				}
+self.eject = function(id) {
+	// are we running on mac?
+	if (process.platform === 'darwin') {
+		// setup optional argument, on mac, will default to 1
+		id = (typeof id === 'undefined') ? 1 : id;
 
-				// if it came here, it went all perfectly and it ejected.
-			});
-		// are we running on linux?
-		} else if (process.platform === 'linux') {
-			// setup optional argument, on linux, will default to '' (empty string)
-			id = (typeof id === 'undefined') ? '' : id;
+		exec('drutil tray eject ' + id, function(err, stdout, stderr) {
+			if (err) {
+				console.log('ERROR: DiskDrive, when executing bash cmd: ' + err);
+				return;
+			}
 
-			exec('eject ' + id, function(err, stdout, stderr) {
-				if (err) {
-					console.log('ERROR: DiskDrive, when executing bash cmd: ' + err);
-					return;
-				}
+			// if it came here, it went all perfectly and it ejected.
+		});
+	// are we running on linux?
+	} else if (process.platform === 'linux') {
+		// setup optional argument, on linux, will default to '' (empty string)
+		id = (typeof id === 'undefined') ? '' : id;
 
-				// if it came here, it went all perfectly and it ejected.
-			});
-		} else {
-			console.log('ERROR: Unsupported DiskDrive platform (' + process.platform + ').');
-		}
+		exec('eject ' + id, function(err, stdout, stderr) {
+			if (err) {
+				console.log('ERROR: DiskDrive, when executing bash cmd: ' + err);
+				return;
+			}
+
+			// if it came here, it went all perfectly and it ejected.
+		});
+	} else {
+		console.log('ERROR: Unsupported DiskDrive platform (' + process.platform + ').');
 	}
 };
